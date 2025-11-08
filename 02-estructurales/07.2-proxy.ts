b19177d782085084e3d0fdcb19faeda5eb5a0bad
anderson <zapataander18@gmail.com>
2025-11-08 16:57:41 -0500

    feat: Enhance Proxy pattern implementation with role-based access control in DocumentProxy

--- 02-estructurales/07.2-proxy.ts
+++ 02-estructurales/07.2-proxy.ts
@@ -34,14 +34,23 @@ class ConfidentialDocument implements Document {
 // 3. Clase Proxy - DocumentProxy
 class DocumentProxy implements Document {
   private document: ConfidentialDocument;
+  private mustHaveRole: string[];
 
-  // TODO: Implementar el constructor de la clase DocumentProxy
+  constructor(document: ConfidentialDocument, mustHaveRole: string[]) {
+    this.document = document;
+    this.mustHaveRole = mustHaveRole;
+  }
 
   displayContent(user: User): void {
-    // TODO: Implementar la lógica para verificar si el usuario tiene permisos
-    // Sólo si es admin puede ver el contenido
-    // Caso contrario, mostrar un mensaje de acceso denegado:
-    // EJ: `%cAcceso denegado. ${user.getName()}, no tienes permisos suficientes para ver este documento.`,
+    const userRole = user.getRole();
+    if (this.mustHaveRole.includes(userRole)) {
+      this.document.displayContent();
+    } else {
+      console.log(
+        `%cAcceso denegado. El usuario ${user.getName()} no tiene permisos para ver este documento.`,
+        COLORS.red
+      );
+    }
   }
 }
 
@@ -70,7 +79,8 @@ function main() {
   const confidentialDoc = new ConfidentialDocument(
     'Este es el contenido confidencial del documento.'
   );
-  const proxy = new DocumentProxy(confidentialDoc);
+
+  const proxy = new DocumentProxy(confidentialDoc, ['admin']);
 
   const user1 = new User('Juan', 'user');
   const user2 = new User('Ana', 'admin');
