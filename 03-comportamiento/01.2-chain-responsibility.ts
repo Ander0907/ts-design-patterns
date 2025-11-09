7e919c0962260bc1a3b0bdd71f101d82feaa130c
anderson <zapataander18@gmail.com>
2025-11-09 16:11:48 -0500

    feat: Implement Chain of Responsibility pattern with Supervisor, Manager, and Director classes for request approval

--- 03-comportamiento/01.2-chain-responsibility.ts
+++ 03-comportamiento/01.2-chain-responsibility.ts
@@ -43,24 +43,32 @@ abstract class BaseApprover implements Approver {
 // 3. Clases Concretas de Aprobadores
 
 class Supervisor extends BaseApprover {
-  // TODO: Implementar el método approveRequest si el monto es menor o igual a 1000
-  // TODO: Si el monto es mayor a 1000, pasar la solicitud al siguiente aprobador
   override approveRequest(amount: number): void {
-    throw new Error('Method not implemented.');
+    if (amount <= 1000) {
+      console.log('%cSupervisor: Aprobando solicitud de $' + amount, COLORS.orange);
+      return;
+    }
+    console.log('%cSupervisor: Pasando solicitud de $' + amount + ' al Manager', COLORS.orange);
+    super.next(amount);
   }
 }
 
 class Manager extends BaseApprover {
-  //TODO: Implementar el método approveRequest si el monto es menor o igual a 5000
-  // TODO: Si el monto es mayor a 5000, pasar la solicitud al siguiente aprobador
-
   override approveRequest(amount: number): void {
-    throw new Error('Method not implemented.');
+    if (amount <= 5000) {
+      console.log('%cManager: Aprobando solicitud de $' + amount, COLORS.yellow);
+      return;
+    }
+    console.log('%cManager: Pasando solicitud de $' + amount + ' al Director', COLORS.yellow);
+    super.next(amount);
   }
 }
 
 class Director extends BaseApprover {
-  // TODO: Implementar el método approveRequest si el monto
+  override approveRequest(amount: number): void {
+    console.log('%cDirector: Aprobando solicitud de $' + amount, COLORS.purple);
+    return;
+  }
 }
 
 // 4. Código Cliente para probar la cadena de responsabilidad
