36ec39bfd71b22532c8d9974f676b18459065a93
anderson <zapataander18@gmail.com>
2025-11-09 14:54:13 -0500

    feat: Implement HomeTheaterFacade with integrated components for a simplified home theater experience; add functionality to watch and end movies

--- 02-estructurales/05.2-facade.ts
+++ 02-estructurales/05.2-facade.ts
@@ -58,18 +58,21 @@ class Memory {
 // 2. Clase Facade - ComputerFacade
 
 class ComputerFacade {
-  // TODO: Agregar los atributos necesarios CPU, Memory y HardDrive
+  private cpu: CPU;
+  private memory: Memory;
+  private hardDrive: HardDrive;
 
-  // TODO: Agregar el constructor para instanciar los atributos CPU, Memory y HardDrive
-  constructor() {}
+  constructor() {
+    this.cpu = new CPU();
+    this.memory = new Memory();
+    this.hardDrive = new HardDrive();
+  }
 
   startComputer(): void {
     console.log('\n%cIniciando la computadora...', COLORS.cyan);
-
-    // TODO: ejecutar las operaciones necesarias para encender la computadora
-    // 1. Cargar el sistema operativo en la memoria - memory.load(0, hardDrive.read(0, 1024))
-    // 2. Saltar a la posición de memoria 0 - cpu.jump(0)
-    // 3. Ejecutar las instrucciones del CPU - cpu.execute()
+    this.memory.load(0, this.hardDrive.read(0, 1024));
+    this.cpu.jump(0);
+    this.cpu.execute();
 
     console.log('Computadora lista para usar.\n');
   }
@@ -77,18 +80,15 @@ class ComputerFacade {
   shutDownComputer(): void {
     console.log('\n%cApagando la computadora...', COLORS.red);
     console.log('Cerrando procesos y guardando datos...');
-
-    // TODO: ejecutar las operaciones necesarias para apagar la computadora
-    // 1. Detener las operaciones del CPU - cpu.stopOperations()
-    // 2. Liberar la memoria - memory.free()
-    // 3. Cerrar el disco duro - hardDrive.close()
+    this.cpu.stopOperations();
+    this.memory.free();
+    this.hardDrive.close();
 
     console.log('Computadora apagada.\n');
   }
 }
 
 // 3. Código Cliente para Usar la Facade
-// TODO: Aquí no hay nada que hacer, debe de encender la computadora y apagarla sin problemas
 function main() {
   const computer = new ComputerFacade();
 
